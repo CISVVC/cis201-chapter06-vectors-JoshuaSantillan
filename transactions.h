4c96a8adb5a4797fd5a0097060c5c25abab2efd2
JoshuaSantillan <37349382+JoshuaSantillan@users.noreply.github.com>
2018-11-12 22:14:43 -0800

    Create transactions.h

---
+++ transactions.h
@@ -0,0 +1,22 @@
+#ifndef TRANSACTIONS_H
+#define TRANSACTIONS_H
+#include<iostream>
+#include<vector>
+#include<string>
+#include "statement.h"
+class Transactions
+{
+  int m_day;
+  double m_transaction;
+  std::string m_description;
+
+public:
+  Transactions();
+  Transactions(int day, double transaction, std::string description);
+  void read();
+  int get_day();
+  double get_transaction();
+  std::string get_description();
+
+};
+#endif
