71dc4815a47b721c77482cff6855d1b355f9fc87
JoshuaSantillan <37349382+JoshuaSantillan@users.noreply.github.com>
2018-11-12 22:14:04 -0800

    Create statement.h

---
+++ statement.h
@@ -0,0 +1,24 @@
+#ifndef STATEMENT_H
+#define STATEMENT_H
+#include<iostream>
+#include<vector>
+#include<string>
+#include "transactions.h"
+class Statement
+{
+
+  std::vector<Transactions> transactions;
+  std::vector<double> daily_balance;
+  double m_balance;
+  double m_min_balance;
+  double m_average_balance;
+  
+public:
+  Statement();
+  void read();
+  void balance(std::vector<Transactions> &v);
+  void print();
+  double min_daily_balance(const std::vector<double> &v);
+  double average_daily_balance(const std::vector<double> &v);
+};
+#endif
