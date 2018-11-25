20b4b04b5c661c30f68a0b66dced8ba2b9e0f973
JoshuaSantillan <37349382+JoshuaSantillan@users.noreply.github.com>
2018-11-24 19:54:34 -0800

    Update statement.cpp

--- statement.cpp
+++ statement.cpp
@@ -9,6 +9,7 @@ Date: Nov 9, 2018
 #include<string>
 #include "statement.h"
 #include "transactions.h"
+
 // statement class functions
 
 //constructor
@@ -41,32 +42,80 @@ void Statement::read()
    balance(transactions);
 }
 //finds the balance for each day and uses pushback to store it
-void Statement::balance(std::vector<Transactions> &v) 
+  std::vector<double> Statement::balance(std::vector<Transactions> &v)
 {
  // std::vector<double> daily_balance;
      m_balance = 0;
 
    for (int i = 0; i < MAX_DAY; i++)
    {
-      m_balance += v[i].get_transaction(); //v[i] is the same as v.at(i)
+      m_balance += sum_total_for_day(i + 1); //v[i] is the same as v.at(i)
    
       daily_balance.push_back(m_balance);
+      
    }
+   return daily_balance;
 }
 
+double Statement::sum_total_for_day(int day)
+{
+  double total = 0;
+  for (int i = 0; i<transactions.size(); i++)
+    {
+      if (day == transactions[i].get_day())
+        {
+          total = total + transactions[i].get_transaction();
+        }
+
+    }
+  return total;
+}
 // prints statement with the data from transaction classes and the balance
 void Statement::print() 
 {
-   for (int i = 0; i < transactions.size(); i++)
+ /* std::cout << "---------" << std::endl;
+   for (int i = 0; i < daily_balance.size(); i++)
    {
-      std::cout << "Day: " << transactions[i].get_day() << " ";
-      std::cout << transactions[i].get_transaction() << " ";
-      std::cout << transactions[i].get_description() << std::endl;
-      std::cout << "Total: " << daily_balance[i] << std::endl;
-   }
-   std::cout << "Minimum interest: " << min_daily_balance(daily_balance) << std::endl;
-   std::cout << "Montly Interest: " << average_daily_balance(daily_balance) << std::endl;
+      if ( i == transactions[i].get_day())
+        {
+          for (int i = 0; i < daily_balance.size(); i++) {
+          std::cout << "Day: " << transactions[i].get_day() << std::endl;
+          std::cout << "Description: " << transactions[i].get_description() << std::endl;
+          }
+        }
+  */
+     int index = 0;
+
+   std::cout << "=====STATEMENT=====" << std::endl;
+   for (int i = 0; i <= MAX_DAY; i++)
+   {
+      if (i == transactions[index].get_day() ) // Having issues printing multiple transactions per day.
+      {
+         
+         std::cout << "================" << std::endl;
+         std::cout << "Day: " << transactions[index].get_day() << std::endl;
+         std::cout << "Transaction: " << transactions[index].get_transaction() << std::endl;
+         std::cout << "Description: " << transactions[index].get_description() << std::endl;
+         std::cout << "================" << std::endl;
+         index++;
+         i++;
+      }
+      std::cout << "Day: " << transactions[index].get_day() << "Balance: " << daily_balance[index] << std::endl;
+      std::cout << "===============" << std::endl;
+    
+   } 
+   
+     
+      
+   /*   std::cout << transactions[index].get_description() << std::endl;
+      std::cout << "Total: " << daily_balance[index] << std::endl;
+      std::cout << "---------" << std::endl;*/
+   
+ 
 }
+   /*std::cout << "Minimum interest: " << min_daily_balance(daily_balance) << std::endl;
+   std::cout << "Montly Interest: " << average_daily_balance(daily_balance) << std::endl; */
+  
 
 double Statement::min_daily_balance(const std::vector<double> &v) // The lowest monthly interest
 {
